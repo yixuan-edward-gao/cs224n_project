037c274de3ce3b5bc8e2333e87d19fdf8d1a432d
Edward Gao <edwardgao@DN0a1e2ec1.SUNet>
2022-02-20 00:59:28 -0800

    alternative self attention

--- models.py
+++ models.py
@@ -92,6 +92,7 @@ class BiDAFCharEmbed(nn.Module):
 
     Args:
         word_vectors (torch.Tensor): Pre-trained word vectors.
+        char_vectors (torch.Tensor): Pre-trained character vectors
         hidden_size (int): Number of features in the hidden state at each layer.
         drop_prob (float): Dropout probability.
     """
@@ -277,6 +278,59 @@ class BiDAFSelfAttention(nn.Module):
         return out
 
 
+class BiDAFRNet(nn.Module):
+
+    def __init__(self, word_vectors, hidden_size, drop_prob=0.):
+        super(BiDAFRNet, self).__init__()
+        self.emb = layers.Embedding(word_vectors=word_vectors,
+                                    hidden_size=hidden_size,
+                                    drop_prob=drop_prob)
+
+        self.enc = layers.RNNEncoder(input_size=hidden_size,
+                                     hidden_size=hidden_size,
+                                     num_layers=1,
+                                     drop_prob=drop_prob)
+
+        self.att = layers.BiDAFAttention(hidden_size=2 * hidden_size,
+                                         drop_prob=drop_prob)
+
+        self.self_att = layers.SelfAttention(input_size= 8 * hidden_size,
+                                             hidden_size=4 * hidden_size,
+                                             num_layers=3,
+                                             drop_prob=drop_prob)
+
+        self.mod = layers.RNNEncoder(input_size=8 * hidden_size,
+                                     hidden_size=hidden_size,
+                                     num_layers=2,
+                                     drop_prob=drop_prob)
+
+        self.out = layers.BiDAFOutput(hidden_size=hidden_size,
+                                      drop_prob=drop_prob)
+
+    def forward(self, cw_idxs, qw_idxs, *args):
+        c_mask = torch.zeros_like(cw_idxs) != cw_idxs
+        q_mask = torch.zeros_like(qw_idxs) != qw_idxs
+        c_len, q_len = c_mask.sum(-1), q_mask.sum(-1)
+
+        c_emb = self.emb(cw_idxs)         # (batch_size, c_len, hidden_size)
+        q_emb = self.emb(qw_idxs)         # (batch_size, q_len, hidden_size)
+
+        c_enc = self.enc(c_emb, c_len)    # (batch_size, c_len, 2 * hidden_size)
+        q_enc = self.enc(q_emb, q_len)    # (batch_size, q_len, 2 * hidden_size)
+
+        att = self.att(c_enc, q_enc,
+                       c_mask, q_mask)    # (batch_size, c_len, 8 * hidden_size)
+
+        att = self.self_att(att)
+
+        mod = self.mod(att, c_len)        # (batch_size, c_len, 2 * hidden_size)
+
+        out = self.out(att, mod, c_mask)  # 2 tensors, each (batch_size, c_len)
+
+        torch.cuda.empty_cache()
+        return out
+
+
 def init_model(name, split, **kwargs):
     name = name.lower()
     if name =='bidaf':
@@ -296,5 +350,9 @@ def init_model(name, split, **kwargs):
         return BiDAFSelfAttention(word_vectors=kwargs['word_vectors'],
                                   hidden_size=kwargs['hidden_size'],
                                   drop_prob=kwargs['drop_prob'] if split == 'train' else 0)
+    elif name == 'rnet':
+        return BiDAFRNet(word_vectors=kwargs['word_vectors'],
+                     hidden_size=kwargs['hidden_size'],
+                     drop_prob=kwargs['drop_prob'] if split == 'train' else 0)
 
     raise ValueError(f'No model named {name}')
