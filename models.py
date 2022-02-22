f7ebef1b59e0ae40e672320e014d39a0aa9d9acf
Edward Gao <edwardgao@DN0a1e2ec1.SUNet>
2022-02-22 00:56:10 -0800

    bug fix; added combined model

--- models.py
+++ models.py
@@ -287,7 +287,8 @@ class BiDAFSelfAttention(nn.Module):
 
         mod = self.mod(self_att, c_len)        # (batch_size, c_len, 2 * hidden_size)
 
-        out = self.out(att, mod, c_mask)  # 2 tensors, each (batch_size, c_len)
+        # out = self.out(att, mod, c_mask)
+        out = self.out(self_att, mod, c_mask)  # 2 tensors, each (batch_size, c_len)
 
         return out
 
@@ -339,6 +340,96 @@ class BiDAFConditionalOutput(nn.Module):
         return out
 
 
+class BiDAFPlus(nn.Module):
+    """
+    BiDAF model plus character embeddings.
+
+    Based on the paper:
+    "Bidirectional Attention Flow for Machine Comprehension"
+    by Minjoon Seo, Aniruddha Kembhavi, Ali Farhadi, Hannaneh Hajishirzi
+    (https://arxiv.org/abs/1611.01603).
+
+    Character level embedding:
+    Using pretrained character vectors, each word is represented as a 2D
+    image of size (vector size x word length).
+    This 2D image is processed by a single convolutional layer and then
+    max-pooled to generate a 1D vector.
+    This 1D vector is concatenated to the projected word embedding vector
+    and then further processed by the highway encoder.
+
+
+    Args:
+        word_vectors (torch.Tensor): Pre-trained word vectors.
+        char_vectors (torch.Tensor): Pre-trained character vectors
+        hidden_size (int): Number of features in the hidden state at each layer.
+        drop_prob (float): Dropout probability.
+    """
+    def __init__(self, word_vectors, char_vectors, hidden_size, drop_prob=0.):
+        super(BiDAFPlus, self).__init__()
+        self.word_emb = layers.WordEmbedding(word_vectors=word_vectors,
+                                             hidden_size=hidden_size,
+                                             drop_prob=drop_prob)
+
+        self.char_emb = layers.CharEmbedding(char_vectors=char_vectors,
+                                             hidden_size=hidden_size,
+                                             drop_prob=drop_prob,
+                                             kernel_height=3)
+
+        self.highway = layers.HighwayEncoder(num_layers=2, hidden_size=2 * hidden_size)
+
+        # shrink the dimension of the output of highway encoder by a factor of 2 to match the rest of the model
+        self.highway_proj = nn.Linear(2 * hidden_size, hidden_size)
+
+        self.enc = layers.RNNEncoder(input_size=hidden_size,
+                                     hidden_size=hidden_size,
+                                     num_layers=1,
+                                     drop_prob=drop_prob)
+
+        self.att = layers.BiDAFAttention(hidden_size=2 * hidden_size,
+                                         drop_prob=drop_prob)
+
+        self.self_att = layers.GatedMultiplicativeSelfAttention(input_size=8 * hidden_size,
+                                                                hidden_size=4 * hidden_size,
+                                                                drop_prob=drop_prob)
+
+        self.mod = layers.RNNEncoder(input_size=8 * hidden_size,
+                                     hidden_size=hidden_size,
+                                     num_layers=2,
+                                     drop_prob=drop_prob)
+
+        self.out = layers.BiDAFOutput(hidden_size=hidden_size,
+                                      drop_prob=drop_prob)
+
+    def forward(self, cw_idxs, qw_idxs, cc_idxs, qc_idxs, *args):
+        c_mask = torch.zeros_like(cw_idxs) != cw_idxs
+        q_mask = torch.zeros_like(qw_idxs) != qw_idxs
+        c_len, q_len = c_mask.sum(-1), q_mask.sum(-1)
+
+        cw_emb = self.word_emb(cw_idxs)         # (batch_size, c_len, hidden_size)
+        qw_emb = self.word_emb(qw_idxs)         # (batch_size, q_len, hidden_size)
+
+        cc_emb = self.char_emb(cc_idxs)         # (batch_size, c_len, hidden_size)
+        qc_emb = self.char_emb(qc_idxs)         # (batch_size, q_len, hidden_size)
+
+        c_emb_cat = torch.cat((cw_emb, cc_emb), 2)  # (batch_size, c_len, 2 * hidden_size)
+        q_emb_cat = torch.cat((qw_emb, qc_emb), 2)  # (batch_size, q_len, 2 * hidden_size)
+
+        c_emb = self.highway_proj(self.highway(c_emb_cat))  # (batch_size, c_len, hidden_size)
+        q_emb = self.highway_proj(self.highway(q_emb_cat))  # (batch_size, q_len, hidden_size)
+
+        c_enc = self.enc(c_emb, c_len)    # (batch_size, c_len, 2 * hidden_size)
+        q_enc = self.enc(q_emb, q_len)    # (batch_size, q_len, 2 * hidden_size)
+
+        att = self.att(c_enc, q_enc, c_mask, q_mask)    # (batch_size, c_len, 8 * hidden_size)
+        self_att = self.self_att(att)
+
+        mod = self.mod(self_att, c_len)        # (batch_size, c_len, 2 * hidden_size)
+
+        out = self.out(self_att, mod, c_mask)  # 2 tensors, each (batch_size, c_len)
+
+        return out
+
+
 def init_model(name, split, **kwargs):
     name = name.lower()
     if name == 'bidaf':
@@ -386,5 +477,10 @@ def init_model(name, split, **kwargs):
                                   drop_prob=kwargs['drop_prob'] if split == 'train' else 0,
                                   att_type='transformer',
                                   n_heads=4)
+    elif name == 'final':
+        return BiDAFPlus(word_vectors=kwargs['word_vectors'],
+                         char_vectors=kwargs['char_vectors'],
+                         hidden_size=kwargs['hidden_size'],
+                         drop_prob=kwargs['drop_prob'] if split == 'train' else 0)
 
     raise ValueError(f'No model named {name}')
